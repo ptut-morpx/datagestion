9dd9a2fa963900a63f0993531144f32e28ea9a1c
Thomas MONTERO <thomas99.montero@gmail.com>
2019-05-08 12:25:46 +0200

    added CSV log support and toID will be a global

--- io/io.py
+++ io/io.py
@@ -1,4 +1,5 @@
 import pickle
+import csv
 
 
 def save(ranking):
@@ -8,9 +9,25 @@ def save(ranking):
 	with open("winner.pickle", "wb") as w:
 		pickle.dump(ranking[0], w, pickle.HIGHEST_PROTOCOL)
 
+	with open("toID.int", "wb") as n:
+		pickle.dump(toID, n, pickle.HIGHEST_PROTOCOL)
+
+
+def logRank(ranking):
+	fileName = "log tournament n{}.csv".format(toID)
+	with open(fileName, 'w', newline='') as csvfile:
+		lineWriter = csv.writer(csvfile, dialect='unix')
+		for player in ranking:
+			lineWriter.writerow([player.name, player.score])
+
+	toID += 1
+
 
 def retrieve():
 	with open("ranking.pickle", "rb") as r:
-		temp = pickle.load(r)
+		ranking = pickle.load(r)
 
-	return temp
+	with open("toID.int", "rb") as n:
+		toID = pickle.load(n)
+
+	return ranking
